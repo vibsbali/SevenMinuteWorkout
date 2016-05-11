f88509bbfaee402cb34a8e37d41df38e3286344c
Vaibhav Bali <vibsbali@gmail.com>
2016-05-11 18:23:19 +1000

    Added partials and created configs

--- SevenMinuteWorkout/app/app.js
+++ SevenMinuteWorkout/app/app.js
@@ -1,5 +1,17 @@
 ﻿"use strict";
 
 (function () {
-    angular.module("app", ["7minWorkout"]);
+    angular.module("app", ["ngRoute", "7minWorkout"])
+        .config(function($routeProvider) {
+            $routeProvider.when("/start", {
+                templateUrl: "/app/SevenMinuteWorkout/partials/start.html"
+            }).when("/workout", {
+                templateUrl: "/app/SevenMinuteWorkout/partials/workout.html",
+                controller: "WorkoutController as vm"
+            }).when("/finish", {
+                templateUrl: "/app/SevenMinuteWorkout/partials/finish.html"
+            }).otherwise({
+                redirectTo: "/start"
+            });
+        });
 }());
